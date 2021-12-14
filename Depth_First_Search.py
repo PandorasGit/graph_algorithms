e1011952eb45bcea89f107dc36967dfdbfaaa109
wendallR <wendallar125@gmail.com>
2021-12-14 12:46:18 -0500

    DFS working

---
+++ Depth_First_Search.py
@@ -0,0 +1,82 @@
+import unittest
+from Vertex import Vertex
+
+
+class DFS:
+    """Class for running DFS and topological sort """
+    time = 0
+
+    def __init__(self, graph):
+        self.graph = graph
+
+
+    def dfs(self):
+        # the time given by wall clock is shared by all
+        # recursive calls
+        def visit(node):
+            self.time += 1
+            node.discovery_time = self.time
+            node.visited = True
+            for v in node.edges:
+                if v.visited is False:
+                    visit(v)
+            self.time += 1
+            node.finishing_time = self.time
+
+        # All nodes are visited!
+        for vertex in self.graph:
+            if not vertex.visited:
+                visit(vertex)
+
+
+    def topological_sort(self):
+        """Sorts keys in graph in descending order by discovery time"""
+
+        self.graph.sort(key=lambda vertex: vertex.finishing_time, reverse=True)
+        return self.graph
+
+
+class TestClass(unittest.TestCase):
+    """
+    Class for testing Implementation
+    """
+
+    @staticmethod
+    def create_graph():
+        """Creates a directed graph for testing"""
+        x = Vertex("x")
+        z = Vertex("z")
+        w = Vertex("w", [z])
+        v = Vertex("v", [w, x])
+        t = Vertex("t")
+        y = Vertex("y", [v])
+        u = Vertex("u", [t])
+        r = Vertex("r", [u, y])
+        q = Vertex("q", [t])
+        m = Vertex("m", [q, r, x])
+        s = Vertex("s", [r])
+        o = Vertex("o", [r, s, v])
+        n = Vertex("n", [q, o])
+        p = Vertex("p", [o, s, z])
+        return [m, q, t, r, u, y, v, w, z, x, n, o, s, p]
+
+    def test_topological_sort(self):
+        dfs_object = DFS(self.create_graph())
+        dfs_object.dfs()
+        array_of_keys = dfs_object.topological_sort()
+        array_test_able = [None for _ in range(len(array_of_keys))]
+
+        for key in array_of_keys:
+            index_of_key = array_of_keys.index(key)
+            array_test_able[index_of_key] = key.name
+
+        correct_solution = ["p", "n", "o", "s", "m", "r", "y", "v", "x", "w", "z", "u", "q", "t"]
+        self.assertEqual(correct_solution, array_test_able)
+
+
+def main():
+    unittest.main()
+
+
+if __name__ == '__main__':
+    main()
---
+++ Vertex.py
@@ -0,0 +1,47 @@
+import unittest
+
+
+class Vertex:
+    """Node in a graph"""
+
+    def __init__(self, name, edges=[]):
+        self.name = name
+        self.edges = edges
+        self.visited = None
+        self.discovery_time = 0
+        self.finishing_time = 0
+        self.visited = False
+
+
+class TestClass(unittest.TestCase):
+    """
+    Class for testing Implementation
+    """
+
+    @staticmethod
+    def create_graph():
+        """Creates a directed graph for testing"""
+        return [Vertex("p", [("o", 1), ("s",), ("z", 1)]), Vertex("n", [("o",), ("u", 1), ("q", 1)]),
+                Vertex("o", [("r", 1), ("s", 1)]), Vertex("s", [("r", 1)]),
+                Vertex("m", [("q", 1), ("r", 1), ("x", 1)]), Vertex("r", [("u", 1), ("y", 1)]),
+                Vertex("y", [("v", 1)]), Vertex("v", [("x", 1), ("w", 1)]),
+                Vertex("w", [("z", 1)]), Vertex("z"), Vertex("u", [("t", 1)]),
+                Vertex("q", [("t", 1)]), Vertex("t")]
+
+    def test_graph_creation_using_vertex(self):
+        graph = self.create_graph()
+        self.assertEqual(graph[0].edges, [("o", 1), ("s",), ("z", 1)])
+
+    def test_graph_change_discovery_times(self):
+        graph = self.create_graph()
+        vertex = graph[1]
+        vertex.discovery_time = 3
+        self.assertNotEqual(vertex.discovery_time, graph[0].discovery_time)
+
+
+def main():
+    unittest.main()
+
+
+if __name__ == '__main__':
+    main()
